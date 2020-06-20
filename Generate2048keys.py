b5f0a78b336efc8693a971668769275ef4153204
mzuppone <msz@msz.eu>
2020-06-20 11:54:04 +0100

    refactoring and readibility enhancements

--- Generate2048keys.py
+++ Generate2048keys.py
@@ -1,35 +1,37 @@
-import socket, binascii, string
+import binascii
+import socket
+import string
 from struct import *
 
 TCP_IP = "192.168.0.36"
 TCP_PORT = 1500
-COMMAND = 'HEADEI2204801%00#0000'
+COMMAND: str = 'HEADEI2204801%00#0000'
 
 
-def testPrintable(str: string) -> string:
+def test_printable(str: string) -> string:
     return all(c in string.printable for c in str)
 
 
-def buildCommand(command):
+def build_command(command):
     # convert hex supplied data into binary 
-    hCommand = ''
+    h_command = ''
     i = 0
     while True:
-        if (command[i:i + 1] == '<'):
+        if command[i:i + 1] == '<':
             i = i + 1
             while True:
-                hCommand = hCommand + binascii.a2b_hex(command[i:i + 2])
+                h_command = h_command + binascii.a2b_hex(command[i:i + 2])
                 i = i + 2
-                if (command[i:i + 1] == '>'):
+                if command[i:i + 1] == '>':
                     i = i + 1
                     break
         else:
-            hCommand = hCommand + command[i]
+            h_command = h_command + command[i]
             i = i + 1
 
         if i == len(command):
             break
-    return hCommand
+    return h_command
 
 
 def main():
@@ -38,23 +40,24 @@ def main():
     global COMMAND
     connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     connection.connect((TCP_IP, TCP_PORT))
+
     BUFFER_SIZE = 1024
 
     # Convert hex to binary
-    COMMAND = buildCommand(COMMAND)
+    COMMAND = build_command(COMMAND)
     # calculate the size and format it correctly
-    SIZE = pack('>h', len(COMMAND))
+    size = pack('>h', len(COMMAND))
     # join everything together 
-    MESSAGE = SIZE + COMMAND
-    # send MESSAGE
-    connection.send(MESSAGE)
+    message = size + COMMAND
+    # send message
+    connection.send(message)
     # receive data
     data = connection.recv(BUFFER_SIZE)
     # don't print ascii if msg or resp contain non printable chars 
-    if (testPrintable(MESSAGE[2:])):
-        print("sent data (ASCII) :", MESSAGE[2:])
-    print("sent data (HEX) :", MESSAGE.encode('hex'))
-    if (testPrintable(data[2:])):
+    if test_printable(message[2:]):
+        print("sent data (ASCII) :", message[2:])
+    print("sent data (HEX) :", message.encode('hex'))
+    if test_printable(data[2:]):
         print("received data (ASCII):", data[2:])
     print("received data (HEX) :", data.encode('hex'))
     connection.close()
